8b506b6c64c782ac0981bf295d863a43404c76b9
Sean Murphy <sean@iamseanmurphy.com>
2026-05-19 20:46:33 +0000

    2.19.0

--- wp-hotelier/hotelier.php
+++ wp-hotelier/hotelier.php
@@ -3,11 +3,11 @@
  * Plugin Name:       WP Hotelier
  * Plugin URI:        https://wphotelier.com/?utm_source=wpadmin&utm_medium=plugin&utm_campaign=wphotelierplugin
  * Description:       Hotel booking plugin for WordPress.
- * Version:           2.18.3
+ * Version:           2.19.0
  * Author:            WP Hotelier
  * Author URI:        https://wphotelier.com/
  * Requires at least: 4.0
- * Tested up to:      6.9
+ * Tested up to:      7.0
  * License:           GPLv3
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       wp-hotelier
@@ -31,7 +31,7 @@ final class Hotelier {
 	/**
 	 * @var string
 	 */
-	public $version = '2.18.3';
+	public $version = '2.19.0';
 
 	/**
 	 * @var Hotelier The single instance of the class
--- wp-hotelier/readme.txt
+++ wp-hotelier/readme.txt
@@ -2,8 +2,8 @@
 Contributors: benitolopez
 Tags: booking, hotel, booking system, hostel, reservations, reservations, b&b, rooms, wphotelier
 Requires at least: 4.1
-Tested up to: 6.9
-Stable tag: 2.18.3
+Tested up to: 7.0
+Stable tag: 2.19.0
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -130,6 +130,9 @@ Here: [WP Hotelier documentation](http://docs.wphotelier.com/).
 4. WP Hotelier - Admin calendar.
 
 == Changelog ==
+
+= 2.19.0 - 2026-05-19 =
+* Add - Support for WordPress 7.0.
 
 = 2.18.3 - 2026-02-27 =
 * Fixed - Apply hotelier_get_all_extras_ids filter when returning early
